355c651d190d723d5a3d731e9ad9d033178fafcf
anil pak <anilpak35@gmail.com>
2021-02-16 10:11:44 +1100

    err fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,10 +4,8 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
-
     if @message.save
-      ChatroomChannel.broadcast_to(@chatroom,
-      render_to_string(partial: "message", locals: { message: @message }))
+      ChatroomChannel.broadcast_to(@chatroom, render_to_string(partial: "message", locals: { message: @message }))
       redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
       render "chatrooms/show"
@@ -15,8 +13,8 @@ class MessagesController < ApplicationController
   end
 
   private
-  
-  def message_params 
+
+  def message_params
     params.require(:message).permit(:content)
   end
 end
